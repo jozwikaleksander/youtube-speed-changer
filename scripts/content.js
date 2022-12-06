60cd1703613941318701173b089e0b04de1206d6
aleksander <aleksanderjozwik@elektronik.edu.pl>
2022-12-06 11:16:00 +0100

    Setting speed after video changes

--- scripts/content.js
+++ scripts/content.js
@@ -4,17 +4,33 @@ console.log('changeSpeed.js loaded');
 
 const videoObj = document.querySelector('video');
 
+const changeSpeed = () => {
+    videoObj.playbackRate = speed;
+    console.log(`Speed set on ${videoObj.playbackRate}`);
+}
+
 chrome.storage.sync.get('speed', (result) => {
     console.log(result);
     if(result != undefined && result.speed != undefined){
         speed = result.speed;
-        videoObj.playbackRate = speed;
-        console.log(`Ustawiono prędkość na ${videoObj.playbackRate}`);
+        changeSpeed();
     }
 })
  
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log(request);
     speed = request.speed;
-    videoObj.playbackRate = speed;
-    console.log(`Ustawiono prędkość 3 na ${speed}`);
+    changeSpeed();
+});
+
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(mutation => {
+        if(mutation.attributeName === 'src'){
+            console.log('src changed');
+            changeSpeed();
+        }
+    })
+});
+observer.observe(videoObj, {
+    attributes: true
 });
